2bcdd89e6d0f1aaef6ffe43fb56d3a6e0e33a981
AtesiT <atesit@mail.ru>
2026-04-30 17:14:47 +0300

    ADD: ability to move on SecondVC

--- SafeData/ContentView.swift
+++ SafeData/ContentView.swift
@@ -1,34 +1,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    //  Переключатель для того, чтобы перейти на второй экран
+    @State private var showSecondView: Bool = false
+    
     @State private var inputUsername: String = ""
     @State private var inputPassword: String = ""
     
     var body: some View {
-        Text("Добро пожаловать!")
-        
-        HStack {
-            Image(systemName: "person.crop.circle")
-                .foregroundStyle(Color.gray)
-            TextField("Введите имя пользователя", text: $inputUsername)
-                .textFieldStyle(.roundedBorder)
+        //  Необходим NavigationStack для того, чтобы можно было осуществлять переход
+        NavigationStack {
+            //  Без VStack могут не работать кнопки (визуально будут отображаться,  но нажатия фиксироваться корректно не будут)
+            VStack {
+                Text("Добро пожаловать!")
+                
+                HStack {
+                    Image(systemName: "person.crop.circle")
+                        .foregroundStyle(Color.gray)
+                    TextField("Введите имя пользователя", text: $inputUsername)
+                        .textFieldStyle(.roundedBorder)
+                    
+                }
+                .padding(.horizontal, 64)
+                
+                HStack {
+                    Image(systemName: "lock.fill")
+                        .foregroundStyle(Color.gray)
+                    TextField("Введите пароль", text: $inputPassword)
+                        .textFieldStyle(.roundedBorder)
+                    
+                }
+                .padding(.horizontal, 64)
+                
+                Button("Войти") {
+                    signIn()
+                }
+            }
+            .padding(8)
+            //  Добавляем переход на второй экран
+            .navigationDestination(isPresented: $showSecondView) {
+                SecondView()
+            }
+        }
+    }
+}
 
+extension ContentView {
+    //  Функция для перехода на второй экран
+    private func signIn() {
+        if (inputUsername == "user") && (inputPassword == "password") {
+            showSecondView = true
         }
-        .padding(.horizontal, 64)
-        
-        HStack {
-            Image(systemName: "lock.fill")
-                .foregroundStyle(Color.gray)
-            TextField("Введите пароль", text: $inputPassword)
-                .textFieldStyle(.roundedBorder)
-
-        }
-        .padding(.horizontal, 64)
-        
-        Button("Войти") {
-            print("Отладка: \(inputUsername) \(inputPassword)")
-        }
-        .padding(8)
     }
 }
 
