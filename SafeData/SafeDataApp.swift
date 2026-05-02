16f2ab0032ef786aca23820a97bc53484e34367a
AtesiT <atesit@mail.ru>
2026-05-02 16:35:20 +0300

    ADD: persistenceController and Environment in main file, DELETE: user struct

--- SafeData/SafeDataApp.swift
+++ SafeData/SafeDataApp.swift
@@ -1,10 +1,16 @@
 import SwiftUI
+import CoreData
 
 @main
 struct SafeDataApp: App {
+    //  Создание экземпляра для управления БД
+    let persistenceController = PersistenceController.shared
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+            //  Environment - передача данных потомкам (вместо того, чтобы вручную в каждом файле прописывать доступ к БД)
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
