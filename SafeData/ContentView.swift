dbe8eea29b9304a6fecf40b6619c06e9d32de43c
AtesiT <atesit@mail.ru>
2026-05-02 12:09:43 +0300

    ADD: background can be changed (if sign in was true or failure), ability to sign in by data in struct Users

--- SafeData/ContentView.swift
+++ SafeData/ContentView.swift
@@ -5,6 +5,8 @@ struct ContentView: View {
     @State private var showSecondView: Bool = false
     //  Переключатель для показа alert
     @State private var showAlert: Bool = false
+    //  Цвет заднего фона
+    @State private var backgroundColor: Color = .clear
     //  Данные пользователей
     @State private var inputUsername: String = ""
     @State private var inputPassword: String = ""
@@ -12,41 +14,47 @@ struct ContentView: View {
     var body: some View {
         //  Необходим NavigationStack для того, чтобы можно было осуществлять переход
         NavigationStack {
-            //  Без VStack могут не работать кнопки (визуально будут отображаться,  но нажатия фиксироваться корректно не будут)
-            VStack {
-                Text("Добро пожаловать!")
-                
-                HStack {
-                    Image(systemName: "person.crop.circle")
-                        .foregroundStyle(Color.gray)
-                    TextField("Введите имя пользователя", text: $inputUsername)
-                        .textFieldStyle(.roundedBorder)
+            //  Устанавливаем ZStack, чтобы установить цвет фона
+            ZStack {
+                backgroundColor
+                    .ignoresSafeArea()
+                    .animation(.default, value: backgroundColor)
+                //  Без VStack могут не работать кнопки (визуально будут отображаться,  но нажатия фиксироваться корректно не будут)
+                VStack {
+                    Text("Добро пожаловать!")
                     
-                }
-                .padding(.horizontal, 64)
-                
-                HStack {
-                    Image(systemName: "lock.fill")
-                        .foregroundStyle(Color.gray)
-                    TextField("Введите пароль", text: $inputPassword)
-                        .textFieldStyle(.roundedBorder)
+                    HStack {
+                        Image(systemName: "person.crop.circle")
+                            .foregroundStyle(Color.gray)
+                        TextField("Введите имя пользователя", text: $inputUsername)
+                            .textFieldStyle(.roundedBorder)
+                        
+                    }
+                    .padding(.horizontal, 64)
                     
+                    HStack {
+                        Image(systemName: "lock.fill")
+                            .foregroundStyle(Color.gray)
+                        SecureField("Введите пароль", text: $inputPassword)
+                            .textFieldStyle(.roundedBorder)
+                        
+                    }
+                    .padding(.horizontal, 64)
+                    
+                    Button("Войти") {
+                        signIn()
+                    }
+                    .alert("Ошибка", isPresented: $showAlert) {
+                        Button("OK", role: .cancel) {}
+                    } message: {
+                        Text("Вы ввели неправильные данные")
+                    }
                 }
-                .padding(.horizontal, 64)
-                
-                Button("Войти") {
-                    signIn()
+                .padding(8)
+                //  Добавляем переход на второй экран
+                .navigationDestination(isPresented: $showSecondView) {
+                    SecondView()
                 }
-                .alert("Ошибка", isPresented: $showAlert) {
-                    Button("OK", role: .cancel) {}
-                } message: {
-                    Text("Вы ввели неправильные данные")
-                }
-            }
-            .padding(8)
-            //  Добавляем переход на второй экран
-            .navigationDestination(isPresented: $showSecondView) {
-                SecondView()
             }
         }
     }
@@ -55,10 +63,13 @@ struct ContentView: View {
 extension ContentView {
     //  Функция для перехода на второй экран
     private func signIn() {
-        if (inputUsername == "user") && (inputPassword == "password") {
+        if (inputUsername.trimmingCharacters(in: .whitespaces) == userOne.username) && (inputPassword.trimmingCharacters(in: .whitespaces) == userOne.password) {
+            backgroundColor = .green
             showSecondView = true
         } else {
+            backgroundColor = .red
             showAlert = true
+            inputPassword = ""
         }
     }
 }
