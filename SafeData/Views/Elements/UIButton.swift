f02420bca89c968c6e89c55f3338c786f7c5807d
AtesiT <atesit@mail.ru>
2026-05-04 20:40:10 +0300

    UPDATE: We can add any action to the UIButton

--- SafeData/Views/Elements/UIButton.swift
+++ SafeData/Views/Elements/UIButton.swift
@@ -5,20 +5,49 @@ struct UIKitButton: UIViewRepresentable {
     //  Текст, который будем передавать кнопке
     var text: String
     var color: UIColor = .systemBlue
+    var action: (() -> Void)?
+    
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
     
     //  Срабатывает один раз
     func makeUIView(context: Context) -> UIButton {
-        let button = UIButton()
-        button.setTitle(text, for: .normal)
-        button.setTitleColor(color, for: .normal)
+        let button = UIButton(type: .system)
+        //  Добавление действия для кнопки
+        //  После нажатия на кнопку, переходит в координатор и вызывает метод tap
+        button.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.tap),
+            for: .touchUpInside
+        )
         return button
     }
     
     //  При обновлении
     func updateUIView(_ uiView: UIButton, context: Context) {
+        uiView.setTitle(text, for: .normal)
+        uiView.setTitleColor(color, for: .normal)
+        //  Когда SwiftUI перерисовывает наш view, мы передаем координатору новую версию нашей структуры (включая action)
+        context.coordinator.parent = self
+    }
+    
+    class Coordinator: NSObject {
+        var parent: UIKitButton
+        
+        init(_ parent: UIKitButton) {
+            self.parent = parent
+        }
+        
+        @objc func tap() {
+            parent.action?()
+        }
     }
 }
 
 #Preview {
-    UIKitButton(text: "Tap me!", color: .black)
+    UIKitButton(text: "Tap me!", color: .black) {
+        print("Tap")
+    }
 }
