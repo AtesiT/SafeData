f590071f8eec3bd18c67b913afa8499b89e72fec
AtesiT <atesit@mail.ru>
2026-05-05 22:22:36 +0300

    ADD: Switch that define the number or symbok that was tapped

--- SafeData/Views/SecondView.swift
+++ SafeData/Views/SecondView.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 struct SecondView: View {
-    @State private var message = "Text"
+    @State private var message = ""
     //  Flexible - позволяет занять колонкам столько места, сколько нужно будет. Но с учётом того, что колонок всего 4 будет.
     let columns = Array(repeating: GridItem(.flexible(), spacing: 8), count: 4)
     
@@ -37,6 +37,30 @@ struct SecondView: View {
     
     private func tapOnButton(_ theButton: String) {
         print("The button \"\(theButton)\" was pressed")
+        
+        switch theButton {
+        case "0": message += "0"
+        case "1": message += "1"
+        case "2": message += "2"
+        case "3": message += "3"
+        case "4": message += "4"
+        case "5": message += "5"
+        case "6": message += "6"
+        case "7": message += "7"
+        case "8": message += "8"
+        case "9": message += "9"
+            
+        case "+": message += "+"
+        case "-": message += "-"
+        case "*": message += "*"
+        case "/": message += "/"
+        
+        case "C": message = ""
+        
+        default:
+            //  It will be a equality =
+            break
+        }
     }
 }
 
