f82a4da9c57670baa7b8f61da8829b66494dac70
AtesiT <atesit@mail.ru>
2026-05-06 11:41:06 +0300

    ADD: ability to display files on ThirdView

--- SafeData/Views/ThirdView.swift
+++ SafeData/Views/ThirdView.swift
@@ -1,8 +1,35 @@
 import SwiftUI
+import UniformTypeIdentifiers
 
 struct ThirdView: View {
+    //  Менеджер сохранения файлов
+    @StateObject private var manager = FileStorageManager()
+    //  Показывать окно импорта файлов
+    @State private var showImportWindow = false
+    
     var body: some View {
-        Text("Third View")
+        //  Для создания заголовка и возможности делать переходы
+        NavigationView {
+            List {
+                //  Через цикл расставляем наши файлы списком
+                ForEach(manager.ourFiles) { file in
+                    //  Делаем слева иконку приложения, названия файла, пустое пространство и справа кнопку, чтобы можно было загрузить
+                    HStack {
+                        Image(systemName: "doc.fill")
+                            .foregroundStyle(.black)
+                        
+                        Text(file.name)
+                        
+                        Spacer()
+                        
+                        ShareLink(item: file.url) {
+                            Image(systemName: "square.and.arrow.up")
+                        }
+                    }
+                }
+            }
+            .navigationTitle("File Manager")
+        }
     }
 }
 
