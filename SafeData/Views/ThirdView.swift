57a531945666e4efb682b3e87ac34ae96aa9c8be
AtesiT <atesit@mail.ru>
2026-05-07 15:20:16 +0300

    EDIT: ThirdView have been edited to MVVM pattern

--- SafeData/Views/ThirdView.swift
+++ SafeData/Views/ThirdView.swift
@@ -2,17 +2,14 @@ import SwiftUI
 import UniformTypeIdentifiers
 
 struct ThirdView: View {
-    //  Менеджер сохранения файлов
-    @StateObject private var manager = FileStorageManager()
-    //  Показывать окно импорта файлов
-    @State private var showImportWindow = false
+    @StateObject private var viewModel = ThirdViewModel()
     
     var body: some View {
         //  Для создания заголовка и возможности делать переходы
         NavigationView {
             List {
                 //  Через цикл расставляем наши файлы списком
-                ForEach(manager.ourFiles) { file in
+                ForEach(viewModel.ourFiles) { file in
                     //  Делаем слева иконку приложения, названия файла, пустое пространство и справа кнопку, чтобы можно было загрузить
                     HStack {
                         Image(systemName: "doc.fill")
@@ -28,40 +25,23 @@ struct ThirdView: View {
                     }
                 }
                 .onDelete { index in
-                    manager.deleteFile(at: index)
+                    viewModel.fileDelete(at: index)
                 }
             }
             .navigationTitle("File Manager")
             .toolbar {
-                Button(action: { showImportWindow = true }) {
+                Button(action: { viewModel.showImportWindow = true }) {
                     Image(systemName: "plus.circle.fill")
                         .font(.title)
                 }
             }
             .fileImporter(
-                //  Если true, то откроется окно добавления файлов
-                isPresented: $showImportWindow,
-                //  Тип файлов (.item - любой тип файлов)
+                isPresented: $viewModel.showImportWindow,
                 allowedContentTypes: [.item],
-                //  Выбираем один файл за раз
-                allowsMultipleSelection: false) { result in
-                    switch result {
-                    //  При успехе, нам вернется массив url
-                    case .success(let urls):
-                        //  Берём первый элемент из массива url
-                        if let url = urls.first {
-                            //  Обращаемся по url, используем метод startAccessingSecurityScopedResource, с помощью которого мы даём нашему приложению прочитать какой-либо файл
-                            if url.startAccessingSecurityScopedResource() {
-                                //  Сохранене файла
-                                manager.saveFile(from: url)
-                                //  Для предотвращения утечек и утечек памяти\лимита файлов
-                                url.stopAccessingSecurityScopedResource()
-                            }
-                        }
-                    case .failure(let error):
-                        print(error.localizedDescription)
-                    }
-                }
+                allowsMultipleSelection: false
+            ) { result in
+                viewModel.fileImport(result: result)
+            }
         }
     }
 }
