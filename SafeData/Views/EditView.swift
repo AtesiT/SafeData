abede3be9af8c873c10a725a1dff0b0980769384
AtesiT <atesit@mail.ru>
2026-05-16 15:25:21 +0300

    EDIT: EditView has been edited to pre-MVVM pattern

--- SafeData/Views/EditView.swift
+++ SafeData/Views/EditView.swift
@@ -3,24 +3,20 @@ import CoreData
 
 struct EditView: View {
     @Environment(\.managedObjectContext) private var viewContext
-    //  Хранятся старые данные в этих переменных
-    @State private var inputUsername: String = ""
-    @State private var inputPassword: String = ""
-    //  Будут хранится новые данные пользователя
-    @State private var newUsername: String = ""
-    @State private var newPassword: String = ""
+
+    @StateObject private var viewModel = EditViewModel()
     
     var body: some View {
         VStack(spacing: 10) {
             Text("Старые данные")
             VStack {
-                TextField("Введите имя", text: $inputUsername)
-                TextField("Введите пароль", text: $inputPassword)
+                TextField("Введите имя", text: $viewModel.inputUsername)
+                SecureField("Введите пароль", text: $viewModel.inputPassword)
             }
             Text("Новые данные")
             VStack {
-                TextField("Введите новое имя", text: $newUsername)
-                TextField("Введите новый пароль", text: $newPassword)
+                TextField("Введите новое имя", text: $viewModel.newUsername)
+                SecureField("Введите новый пароль", text: $viewModel.newPassword)
             }
             Button("Изменить данные") {
                 editUser()
@@ -32,10 +28,10 @@ struct EditView: View {
 extension EditView {
     private func editUser() {
         //  Присваиваем переменные
-        let correctUsername = inputUsername.trimmingCharacters(in: .whitespaces)
-        let correctPassword = hashInfo(inputPassword.trimmingCharacters(in: .whitespaces))
-        let newCorrectUsername = newUsername.trimmingCharacters(in: .whitespaces)
-        let newCorrectPassword = hashInfo(newPassword.trimmingCharacters(in: .whitespaces))
+        let correctUsername = viewModel.inputUsername.trimmingCharacters(in: .whitespaces)
+        let correctPassword = hashInfo(viewModel.inputPassword.trimmingCharacters(in: .whitespaces))
+        let newCorrectUsername = viewModel.newUsername.trimmingCharacters(in: .whitespaces)
+        let newCorrectPassword = hashInfo(viewModel.newPassword.trimmingCharacters(in: .whitespaces))
         
         //  Запрос к базе данных
         let request = NSFetchRequest<User>(entityName: "User")
--- SafeData/Views/EditViewModel.swift
+++ SafeData/Views/EditViewModel.swift
@@ -1,1 +1,12 @@
+import SwiftUI
+import Combine
 
+class EditViewModel: ObservableObject {
+    //  Переменные, которые будут использоваться
+    @Published var inputUsername: String = ""
+    @Published var inputPassword: String = ""
+    //  Будут хранится новые данные пользователя
+    @Published var newUsername: String = ""
+    @Published var newPassword: String = ""
+
+}
