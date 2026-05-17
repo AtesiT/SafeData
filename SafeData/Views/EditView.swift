4a59dff3d798e919166b03673c7ff59b23f21ab1
AtesiT <atesit@mail.ru>
2026-05-17 17:37:32 +0300

    EDIT: EditView has been edited to MVVM-pattern

--- SafeData/Views/EditView.swift
+++ SafeData/Views/EditView.swift
@@ -4,7 +4,11 @@ import CoreData
 struct EditView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
-    @StateObject private var viewModel = EditViewModel()
+    @StateObject private var viewModel: EditViewModel
+    
+    init() {
+        _viewModel = StateObject(wrappedValue: EditViewModel(viewContext: PersistenceController.shared.container.viewContext))
+    }
     
     var body: some View {
         VStack(spacing: 10) {
@@ -19,47 +23,13 @@ struct EditView: View {
                 SecureField("Введите новый пароль", text: $viewModel.newPassword)
             }
             Button("Изменить данные") {
-                editUser()
+                viewModel.editUser()
             }
         }
-    }
-}
-
-extension EditView {
-    private func editUser() {
-        //  Присваиваем переменные
-        let correctUsername = viewModel.inputUsername.trimmingCharacters(in: .whitespaces)
-        let correctPassword = hashInfo(viewModel.inputPassword.trimmingCharacters(in: .whitespaces))
-        let newCorrectUsername = viewModel.newUsername.trimmingCharacters(in: .whitespaces)
-        let newCorrectPassword = hashInfo(viewModel.newPassword.trimmingCharacters(in: .whitespaces))
-        
-        //  Запрос к базе данных
-        let request = NSFetchRequest<User>(entityName: "User")
-        
-        //  Фильтруем
-        request.predicate = NSPredicate(format: "username == %@ AND password == %@", correctUsername, correctPassword)
-        //  Возвращение только одного результата
-        request.fetchLimit = 1
-        
-        //  Присваиваем данные
-        let result = (try? viewContext.fetch(request)) ?? []
-        
-        if let theUser = result.first {
-            //  Присваиваем новые данные пользователю
-            theUser.username = newCorrectUsername
-            theUser.password = newCorrectPassword
-            
-            //  Если БД была изменена, то оповещаем и сохраняем новые данные
-            if viewContext.hasChanges {
-                do {
-                    try viewContext.save()
-                    print("Данные пользователя были изменены")
-                } catch {
-                    print(error.localizedDescription)
-                }
-            }
-        } else {
-            print("Ошибка")
+        .alert("Вывод", isPresented: $viewModel.showAlert) {
+            Button("OK", role: .cancel) {}
+        } message: {
+            Text(viewModel.alertMessage)
         }
     }
 }
--- SafeData/Views/EditViewModel.swift
+++ SafeData/Views/EditViewModel.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 import Combine
+import CoreData
 
 class EditViewModel: ObservableObject {
     //  Переменные, которые будут использоваться
@@ -8,5 +9,56 @@ class EditViewModel: ObservableObject {
     //  Будут хранится новые данные пользователя
     @Published var newUsername: String = ""
     @Published var newPassword: String = ""
+    
+    @Published var alertMessage = ""
+    @Published var showAlert = false
 
+    private let viewContext: NSManagedObjectContext
+    
+    init(viewContext: NSManagedObjectContext) {
+        self.viewContext = viewContext
+    }
+}
+
+extension EditViewModel {
+    func editUser() {
+        //  Присваиваем переменные
+        let correctUsername = inputUsername.trimmingCharacters(in: .whitespaces)
+        let correctPassword = hashInfo(inputPassword.trimmingCharacters(in: .whitespaces))
+        let newCorrectUsername = newUsername.trimmingCharacters(in: .whitespaces)
+        let newCorrectPassword = hashInfo(newPassword.trimmingCharacters(in: .whitespaces))
+        
+        //  Запрос к базе данных
+        let request = NSFetchRequest<User>(entityName: "User")
+        
+        //  Фильтруем
+        request.predicate = NSPredicate(format: "username == %@ AND password == %@", correctUsername, correctPassword)
+        //  Возвращение только одного результата
+        request.fetchLimit = 1
+        
+        //  Присваиваем данные
+        let result = (try? viewContext.fetch(request)) ?? []
+        
+        if let theUser = result.first {
+            //  Присваиваем новые данные пользователю
+            theUser.username = newCorrectUsername
+            theUser.password = newCorrectPassword
+            
+            //  Если БД была изменена, то оповещаем и сохраняем новые данные
+            if viewContext.hasChanges {
+                do {
+                    try viewContext.save()
+                    self.alertMessage = "Данные пользователя успешно отредактированы"
+                    self.showAlert = true
+                    self.inputUsername = ""
+                    self.inputPassword = ""
+                } catch {
+                    self.alertMessage = error.localizedDescription
+                    self.showAlert = true
+                }
+            }
+        } else {
+            print("Ошибка")
+        }
+    }
 }
